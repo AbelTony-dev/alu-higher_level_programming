e558669ca08accc231305ecebeca8a38642f2b07
AbelTony-dev <a.niyindagi@alustudent.com>
2023-02-02 00:35:54 -0800

    if loop fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,4 @@
-for i in range(100):
-    print("{:02d}".format(i), end=", " if i < 99 else "\n")
+print("{:02d}, ".format(0), end="")
+for i in range(1, 99):
+    print("{:02d}, ".format(i), end="")
+print("{:02d}".format(99))
