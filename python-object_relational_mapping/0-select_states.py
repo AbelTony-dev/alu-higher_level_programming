1a3d2842d66fbc71a385a4a2301fd674a79ca049
Abel Tony <122355309+AbelTony-dev@users.noreply.github.com>
2023-04-28 23:58:08 +0200

    wwww

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,38 @@
-#!/usr/bin/python3
-# Import necessary modules
-"""module documentation"""
+#!/usr/bin/env python3
 import MySQLdb
 import sys
 
-# Connect to MySQL database using command-line arguments
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+    # Check if all required arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
 
-    # Create cursor object to execute SQL queries
+    # Get the command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+
+    # Create a cursor to execute SQL queries
     cursor = db.cursor()
 
-    # Execute SQL query to select all states and order by id
-    cursor.execute('SELECT * FROM states ORDER BY id ASC')
+    # Execute the SQL query to select all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all rows and print them out
+    # Fetch all the rows from the query result
     rows = cursor.fetchall()
+
+    # Print the results
     for row in rows:
         print(row)
 
-    # Close cursor and database connections
+    # Close the cursor and database connections
     cursor.close()
     db.close()
